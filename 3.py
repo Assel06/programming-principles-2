dc52df72fd1f04dd052d01eb2d44701069a1113a
Assel06 <123243504+Assel06@users.noreply.github.com>
2023-04-14 23:52:18 +0600

    ball(lab8)

--- 3.py
+++ 3.py
@@ -1,34 +1,41 @@
-from tkinter import *
 import pygame
-import os
-
-root = Tk()
-root.title('MUsic Player')
-root.geometry("500x300")
-
-pygame.mixer.init()
-
-songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
-songlist.pack()
-
-play_btn_image = PhotoImage(file='play.png')
-pause_btn_image = PhotoImage(file='pause.png')
-next_btn_image = PhotoImage(file='next.png')
-rewind_btn_image = PhotoImage(file='rewind.png')
-
-control_frame = Frame(root)
-control_frame.pack()
-
-play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
-pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
-next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
-rewind_btn = Button(control_frame, image=rewind_btn_image, borderwidth=0)
-
-play_btn.grid(row = 0, column = 0, padx = 2, pady = 3)
-pause_btn.grid(row = 0, column = 1, padx = 7, pady = 10)
-next_btn.grid(row = 0, column = 2, padx = 7, pady = 10)
-rewind_btn.grid(row = 0, column = 3, padx = 7, pady = 10)
 
 
+pygame.init()
+width = 490
+lenght = 490
+screen = pygame.display.set_mode((width, lenght))
 
-root.mainloop()
+done = False
+
+x = lenght / 2
+y = width / 2
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            done = True
+    pressed = pygame.key.get_pressed()
+
+    
+    if pressed[pygame.K_UP] and y >= 30:
+        y -= 20
+    if pressed[pygame.K_DOWN] and y <= width - 30:
+        y += 20
+    if pressed[pygame.K_LEFT] and x >= 30:
+        x -= 20
+    if pressed[pygame.K_RIGHT] and x < lenght - 30:
+        x +=20
+
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (255, 0, 0),(x, y), 25, 0)
+    
+    pygame.display.flip()
+    clock.tick(60)
+
+
+
+
